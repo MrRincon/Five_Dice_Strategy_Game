374219b7affe177ef11e7c9b017e04d4c55fe220
MrRincon <alamrdrgz98@gmail.com>
2025-05-05 19:55:47 +0100

    Removing old files

--- player.java
+++
@@ -1,8 +0,0 @@
-package summativeassesment1;
-
-public class player {
-
-    //Create Obj for the player board values and for the selections made in the game
-    int[] boardValues = new int[8];
-    int[] comboSelection = new int[7];
-}
